fc8a638db61326eda0aeb3faa50bdd6bdd29f90e
guwazhe <guwazhe@gmail.com>
2025-01-04 17:19:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def main(input_email: str):
 def task():
     main(input_email)
 if __name__ == "__main__":
-    global times
+    global times, input_token, input_chatid
     times = 1
     del_ocr()
     os.makedirs("static", exist_ok=True)
