0b02fb8206f8e1261e9acd299f1f33505bb99156
guwazhe <guwazhe@gmail.com>
2025-01-05 09:11:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def main(input_email: str):
                 "referer": "https://www.serv00.com/offer/create_new_account",
                 "accept-encoding": "gzip, deflate",
                 "accept-language": "en-US;q\u003d0.8,en;q\u003d0.7",
-                "cookie": Cookie,
+                "cookie": Cookie
             }
             errors = 1
             email = input_email
